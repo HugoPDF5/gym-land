f5808b8d8ad74a073d0481f11b8479dcb77cd72e
Hugo <hugopatricio51@gmail.com>
2023-04-04 13:54:54 -0300

    feat: finalizing header and adding SASS

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Header from '../Header/Header'
+import Hero_image from '../../assets/hero_image.png'
+import Hero_image_back from '../../assets/hero_image_back.png'
+import Heart from '../../assets/heart.png'
+import Calories from '../../assets/calories.png'
 import './hero.scss'
 
 const Hero = () => {
@@ -22,12 +26,53 @@ const Hero = () => {
                         <span>Ideal body</span>
                     </div>
                     <div>
-                        <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+                        <span>
+                            In here we will help you to shape and build your
+                            ideal body and live up your life to fullest
+                        </span>
+                    </div>
+                </div>
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>expert coaches</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+                <div className="hero-buttons">
+                    <button className="btn">Get Started</button>
+                    <button className="btn">Learn More</button>
+                </div>
+
+
+            </div>
+            <div className="right-h">
+                <button className='btn'>Join Now</button>
+
+                <div className="heart-rate">
+                    <img src={Heart} alt="" />
+                    <span>Heart rate</span>
+                    <span>116 BPM</span>
+                </div>
+
+                <img src={Hero_image} className='hero-image' alt="" />
+                <img src={Hero_image_back} className='hero-image-back' alt="" />
+
+                <div className="calories">
+                    <img src={Calories} alt="" />
+                    <div>
+                        <span>Calories Burned</span>
+                        <span>220kcal</span>
                     </div>
                 </div>
             </div>
-
-            <div className="right-h">right side</div>
         </div>
     )
 }
